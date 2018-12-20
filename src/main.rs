be0f6763759182da74f13673f279b379638d113e
laopo001 <353272497@qq.com>
2018-12-20 22:01:59 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+use std::collections::HashMap;
+
 fn num_unique_emails(emails: &mut Vec<String>) -> i32 {
+    let map: HashMap<&str, bool> = HashMap::new();
     for item in emails.into_iter() {
         let arr: Vec<_> = item.split('@').collect();
         let left = arr[0];
         let right = arr[1];
-        
     }
     return 123;
 }
 
 fn main() {
-
     let mut emails = vec!["123".to_string()];
     num_unique_emails(&mut emails);
     println!("Hello, world!");
