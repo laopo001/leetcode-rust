7bf3c39b84876be9c2d917cb6a90c3c327984324
laopo001 <353272497@qq.com>
2018-12-24 15:15:26 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,96 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 fn num_unique_emails(emails: &mut Vec<String>) -> i32 {
-    let map: HashMap<&str, bool> = HashMap::new();
+    let mut set: HashSet<String> = HashSet::new();
     for item in emails.into_iter() {
-        let arr: Vec<_> = item.split('@').collect();
-        let left = arr[0];
-        let right = arr[1];
+        let arr: Vec<_> = item.split("@").collect();
+        let mut nemail = String::new();
+        let left: &str = arr[0];
+        let right: &str = arr[1];
+        let mut t: String = left.replace(".", "");
+        let arr2: Vec<_> = t.match_indices("+").collect();
+        if arr2.len() > 0 {
+            t = t[0..arr2[0].0].to_string();
+        }
+        nemail = t + "@" + right;
+        set.insert(nemail);
+    }
+    return set.len() as i32;
+}
+
+fn repeated_n_times(a: Vec<i32>) -> i32 {
+    let mut map: HashMap<i32, bool> = HashMap::new();
+    for item in a {
+        if (!map.contains_key(&item)) {
+            map.insert(item, true);
+        } else {
+            return item;
+        }
     }
     return 123;
 }
 
+fn to_lower_case(str: String) -> String {
+    // for i in 0..str.len() {
+    //     let c = str.get(i..(i+1));
+    // }
+    let arr = str.as_bytes();
+    let mut res = Vec::new();
+    for item in arr {
+        if *item <= (65 as u8) && *item <= (90 as u8) {
+            res.push(item + 32);
+        } else {
+            res.push(*item);
+        }
+    }
+    return String::from_utf8(res).unwrap();
+}
+
 fn main() {
-    let mut emails = vec!["123".to_string()];
+    let mut emails = vec!["12@3".to_string()];
+    let map: HashMap<String, bool> = HashMap::new();
     num_unique_emails(&mut emails);
     println!("Hello, world!");
+}
+
+#[derive(Debug, Copy, Clone)]
+enum EmailParseState {
+    Name,
+    Plus,
+    Domain,
+}
+
+#[derive(Debug)]
+struct Solution {}
+impl Solution {
+    pub fn num_unique_emails(emails: Vec<String>) -> i32 {
+        let mut unique_emails = HashSet::new();
+        for email in emails {
+            let nemail = Solution::parse_email(&email);
+            unique_emails.insert(nemail);
+        }
+        unique_emails.len() as i32
+    }
+
+    fn parse_email(email: &String) -> String {
+        let mut nemail = String::new();
+        let mut state = EmailParseState::Name;
+
+        for c in email.chars() {
+            let (new_state, ch) = match (state, c) {
+                (EmailParseState::Name, '.') => (state, None),
+                (EmailParseState::Name, '+') => (EmailParseState::Plus, None),
+                (EmailParseState::Name, '@') => (EmailParseState::Domain, Some(c)),
+                (EmailParseState::Name, _) => (state, Some(c)),
+                (EmailParseState::Plus, '@') => (EmailParseState::Domain, Some(c)),
+                (EmailParseState::Plus, _) => (state, None),
+                (EmailParseState::Domain, _) => (state, Some(c)),
+            };
+            if let Some(c) = ch {
+                nemail.push(c);
+            }
+            state = new_state;
+        }
+        nemail
+    }
 }
