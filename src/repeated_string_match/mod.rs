3e41b6e6455fd48b89684d8ea1209afcc6dd7ba8
laopo001 <353272497@qq.com>
2019-01-23 12:02:23 +0800

    fix repeated_string_match

--- src/repeated_string_match/mod.rs
+++ src/repeated_string_match/mod.rs
@@ -4,9 +4,14 @@ use crate::base::{Solution, TreeNode};
 impl Solution {
     pub fn repeated_string_match(a: String, b: String) -> i32 {
         let mut s = a.clone();
-        let len2 = b.len();
         let mut res = 1;
-        while s.len() < len2 || (a.len() != 1 && s.len() <= len2) {
+        while s.len() < b.len() {
+            s = s + a.as_str();
+            res = res + 1;
+        }
+        if s.as_str().contains(b.as_str()) {
+            return res;
+        } else {
             s = s + a.as_str();
             res = res + 1;
         }
