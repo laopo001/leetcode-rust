dd6b0c58569ffc06b46c6cfdd7a3c9bf487863a9
laopo001 <353272497@qq.com>
2019-02-08 20:31:53 +0800

    fix binary_tree_maximum_path_sum

--- src/binary_tree_maximum_path_sum/mod.rs
+++ src/binary_tree_maximum_path_sum/mod.rs
@@ -5,35 +5,36 @@ use std::cell::RefCell;
 use std::cmp::max;
 use std::rc::Rc;
 
+
 fn run(root: Option<Rc<RefCell<TreeNode>>>, res: &mut i32) -> i32 {
     if root == None {
         return 0;
     }
     let node = root.unwrap();
-
-    let mut l = run(node.borrow().left.clone(), res);
-    let mut r = run(node.borrow().right.clone(), res);
-    if node.borrow().left.clone() != None
-        && node.borrow().val == node.borrow().left.clone().unwrap().borrow().val
-    {
-        l += 1;
-    } else {
-        l = 0;
+    if node.borrow().left.clone() == None && node.borrow().right.clone() == None {
+        *res = max(*res, node.borrow().val);
+        return node.borrow().val;
     }
-    if node.borrow().right.clone() != None
-        && node.borrow().val == node.borrow().right.clone().unwrap().borrow().val
-    {
-        r += 1;
-    } else {
-        r = 0;
-    }
-    *res = max(*res, l + r);
-    return max(l, r);
+    let l = run(node.borrow().left.clone(), res);
+    let r = run(node.borrow().right.clone(), res);
+    let maxValue = max(
+        l + r + node.borrow().val,
+        max(
+            node.borrow().val,
+            max(l + node.borrow().val, r + node.borrow().val),
+        ),
+    );
+    *res = max(*res, maxValue);
+    return max(
+            node.borrow().val,
+            max(l + node.borrow().val, r + node.borrow().val),
+        );
 }
-
 
 impl Solution {
     pub fn max_path_sum(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        return 123;
+        let mut res = std::i32::MIN;
+        run(root, &mut res);
+        return res;
     }
 }
