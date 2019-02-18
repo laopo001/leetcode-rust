f26f01218d3e61d3590bf044154da089fa1d4fdf
laopo001 <353272497@qq.com>
2019-02-18 19:27:26 +0800

    fix best_time_to_buy_and_sell_stock

--- src/best_time_to_buy_and_sell_stock/mod.rs
+++ src/best_time_to_buy_and_sell_stock/mod.rs
@@ -5,6 +5,14 @@ use std::cmp::max;
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let mut res = 0;
+        let mut max_value = std::i32::MAX;
+        for item in prices {
+            if max_value > item {
+                max_value = item;
+            } else {
+                res = max(res, item - max_value);
+            }
+        }
         return res;
     }
 }
