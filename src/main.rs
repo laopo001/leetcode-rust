918b3b27d9f6022e9ab31193c862bc96f5153900
laopo001 <353272497@qq.com>
2019-02-24 23:38:44 +0800

    fix number_complement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 #![allow(unused)]
-use leetcode::base::Solution;
+use leetcode::base::{string_to_num, to_decimal, Solution};
 use leetcode::best_time_to_buy_and_sell_stock_with_cooldown;
 
 fn main() {
-    Solution::max_profit3(vec![2,6,5,0,3]);
+    Solution::max_profit3(vec![2, 6, 5, 0, 3]);
+    let mut s = to_decimal(5, 2);
 
+    let mut temp = String::new();
+
+    for c in s.bytes() {
+        if c == b'0' {
+            temp.push('1');
+        } else {
+            temp.push('0');
+        }
+    }
+
+    let mut res = string_to_num(temp, 2);
+    println!("{:?}", res);
     println!("Hello, world!");
 }
--- src/number_complement/mod.rs
+++ src/number_complement/mod.rs
@@ -1,8 +1,39 @@
 #![allow(unused)]
 use crate::base::{Solution, TreeNode};
 
+pub fn to_decimal(n: i32, d: i32) -> String {
+    let mut res = "".to_string();
+    let mut n = n;
+    while n != 0 {
+        let t = n % d;
+        res = t.to_string() + res.as_str();
+        n = (n - t) / d;
+    }
+    return res.to_string();
+}
+
+pub fn string_to_num(s: String, d: i32) -> i32 {
+    let mut res = 0;
+    let bytes = s.as_bytes();
+    for i in 0..bytes.len() {
+        let index = bytes.len() - 1 - i;
+        res += (bytes[i] as i32 - 48) * d.pow(index as u32);
+    }
+    return res;
+}
+
 impl Solution {
     pub fn find_complement(num: i32) -> i32 {
-        return 123;
+        let mut s = to_decimal(num, 2);
+        let mut temp = String::new();
+
+        for c in s.bytes() {
+            if c == b'0' {
+                temp.push('1');
+            } else {
+                temp.push('0');
+            }
+        }
+        return string_to_num(temp, 2);
     }
 }
