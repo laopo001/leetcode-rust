7a2a041c144e10c55bec6065d067d2a1651f4f4c
laopo001 <353272497@qq.com>
2019-03-25 01:16:43 +0800

    fix merge_sorted_array

--- src/merge_sorted_array/mod.rs
+++ src/merge_sorted_array/mod.rs
@@ -1,7 +1,41 @@
 use crate::base::Solution;
+// trait JsArray<T> {
+//     // 方法从数组中删除第一个元素
+//     fn shift(&mut self) -> Option<T> {
+//         return self.remove_item(&0);
+//     }
+//     fn unshift(&mut self, elm: T);
+// }
 
 impl Solution {
+    #[allow(while_true)]
     pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-        
+        let mut len = m + n - 1;
+        let mut j = n - 1;
+        let mut i = m - 1;
+        while true {
+            if i < m && i >= 0 && j < n && j >= 0 {
+                if nums1[i as usize] > nums2[j as usize] {
+                    nums1[len as usize] = nums1[i as usize];
+                    len -= 1;
+                    i -= 1;
+                } else {
+                    nums1[len as usize] = nums2[j as usize];
+                    len -= 1;
+                    j -= 1;
+                }
+            } else if i < m && i >= 0 {
+                nums1[len as usize] = nums1[i as usize];
+                len -= 1;
+                i -= 1;
+            } else if j < n && j >= 0 {
+                nums1[len as usize] = nums2[j as usize];
+                len -= 1;
+                j -= 1;
+            }
+            if len < 0 {
+                break;
+            }
+        }
     }
 }
