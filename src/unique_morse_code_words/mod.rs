cf836f0b160c6bc2640c501f3b98a05a0d265e10
laopo001 <353272497@qq.com>
2019-03-31 20:33:00 +0800

    fix unique_morse_code_words

--- src/unique_morse_code_words/mod.rs
+++ src/unique_morse_code_words/mod.rs
@@ -1,7 +1,22 @@
 use crate::base::Solution;
+use std::collections::HashSet;
 
 impl Solution {
     pub fn unique_morse_representations(words: Vec<String>) -> i32 {
-        return 0;
+        let arr = [
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
+            "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+            "-.--", "--..",
+        ];
+        let mut set: HashSet<String> = HashSet::new();
+        for s in words {
+            let mut temp = "".to_string();
+            for b in s.as_bytes() {
+                let index = *b - b'a';
+                temp += arr[index as usize];
+            }
+            set.insert(temp);
+        }
+        return set.len() as i32;
     }
 }
