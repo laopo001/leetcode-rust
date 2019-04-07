bd711f0b954bc716e3a69e825f3cf1ba621067a2
laopo001 <353272497@qq.com>
2019-04-07 23:48:36 +0800

    fix merge_two_binary_trees

--- src/merge_two_binary_trees/mod.rs
+++ src/merge_two_binary_trees/mod.rs
@@ -7,6 +7,36 @@ impl Solution {
         t1: Option<Rc<RefCell<TreeNode>>>,
         t2: Option<Rc<RefCell<TreeNode>>>,
     ) -> Option<Rc<RefCell<TreeNode>>> {
-        return t1;
+        if t1.is_none() && t2.is_none() {
+            return None;
+        } else if t1.is_some() && t2.is_none() {
+            let t_clone = t1.clone().unwrap();
+            let t = t_clone.borrow().val;
+            let mut node = TreeNode::new(t);
+            node.left = Solution::merge_trees(t_clone.borrow().left.clone(), None);
+            node.right = Solution::merge_trees(t_clone.borrow().right.clone(), None);
+            return Some(Rc::new(RefCell::new(node)));
+        } else if t1.is_none() && t2.is_some() {
+            let t2_clone = t2.clone().unwrap();
+            let t = t2_clone.borrow().val;
+            let mut node = TreeNode::new(t);
+            node.left = Solution::merge_trees(None, t2_clone.borrow().left.clone());
+            node.right = Solution::merge_trees(None, t2_clone.borrow().right.clone());
+            return Some(Rc::new(RefCell::new(node)));
+        } else {
+            let t_clone = t1.clone().unwrap();
+            let t2_clone = t2.clone().unwrap();
+            let t = t_clone.borrow().val + t2_clone.borrow().val;
+            let mut node = TreeNode::new(t);
+            node.left = Solution::merge_trees(
+                t_clone.borrow().left.clone(),
+                t2_clone.borrow().left.clone(),
+            );
+            node.right = Solution::merge_trees(
+                t_clone.borrow().right.clone(),
+                t2_clone.borrow().right.clone(),
+            );
+            return Some(Rc::new(RefCell::new(node)));
+        }
     }
 }
