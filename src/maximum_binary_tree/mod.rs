03c6b1d325ae3e2004fbd9423993a8c1c92521b2
laopo001 <353272497@qq.com>
2019-04-10 01:14:36 +0800

    fix maximum_binary_tree

--- src/maximum_binary_tree/mod.rs
+++ src/maximum_binary_tree/mod.rs
@@ -2,8 +2,34 @@ use crate::base::{Solution, TreeNode};
 
 use std::cell::RefCell;
 use std::rc::Rc;
+
+fn find_max_index(nums: &Vec<i32>) -> Option<(i32, usize)> {
+    if nums.len() == 0 {
+        return None;
+    }
+    let mut index = (i32::min_value(), 0);
+    for i in 0..nums.len() {
+        if nums[i] > index.0 {
+            index.0 = nums[i];
+            index.1 = i;
+        }
+    }
+    return Some(index);
+}
+
 impl Solution {
     pub fn construct_maximum_binary_tree(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-        return None;
+        let index = find_max_index(&nums);
+        if index == None {
+            return None;
+        } else {
+            let (val, i) = index.unwrap();
+            let mut node = TreeNode::new(val);
+            let left: Vec<i32> = nums.split_at(i).0.into();
+            node.left = Solution::construct_maximum_binary_tree(left);
+            let right: Vec<i32> = nums.split_at(i + 1).1.into();
+            node.right = Solution::construct_maximum_binary_tree(right);
+            return Some(Rc::new(RefCell::new(node)));
+        }
     }
 }
