467bea6b4ede71e3fc6269efe411c2a3a8f97e15
laopo001 <353272497@qq.com>
2019-04-11 00:04:03 +0800

    fix remove_outermost_parentheses

--- src/remove_outermost_parentheses/mod.rs
+++ src/remove_outermost_parentheses/mod.rs
@@ -2,6 +2,22 @@ use crate::base::Solution;
 
 impl Solution {
     pub fn remove_outer_parentheses(s: String) -> String {
-        return s;
+        let mut res = "".to_string();
+        let mut count = 0;
+        for i in s.as_bytes() {
+            if *i == b'(' {
+                if count != 0 {
+                    res += &std::str::from_utf8(&[*i]).unwrap();
+                }
+                count += 1;
+            }
+            if *i == b')' {
+                count -= 1;
+                if count != 0 {
+                    res += &std::str::from_utf8(&[*i]).unwrap();
+                }
+            }
+        }
+        return res;
     }
 }
