7137c22ad1fa269180d6ee1dba7305fc9d69ec27
laopo001 <353272497@qq.com>
2019-04-13 23:59:44 +0800

    fix array_partition_i

--- src/array_partition_i/mod.rs
+++ src/array_partition_i/mod.rs
@@ -4,7 +4,10 @@ impl Solution {
     pub fn array_pair_sum(nums: Vec<i32>) -> i32 {
         let mut nums = nums;
         nums.sort();
-   
-        return 0;
+        let mut res = 0;
+        for chunk in nums.as_slice().chunks(2) {
+            res += std::cmp::min(chunk[0], chunk[1]);
+        }
+        return res;
     }
 }
