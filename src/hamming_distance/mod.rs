7b81e69d1353a04351c17afd3153933634f51d9e
laopo001 <353272497@qq.com>
2019-04-14 22:42:04 +0800

    fix hamming_distance

--- src/hamming_distance/mod.rs
+++ src/hamming_distance/mod.rs
@@ -2,7 +2,6 @@ use crate::base::Solution;
 
 impl Solution {
     pub fn hamming_distance(x: i32, y: i32) -> i32 {
-        let mut res = 0;
-        return res;
+        (x ^ y).count_ones() as i32
     }
 }
