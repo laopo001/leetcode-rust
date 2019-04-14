73cc112c3d2f017faaa8b217591968b71efc2c79
laopo001 <353272497@qq.com>
2019-04-14 22:29:36 +0800

    add hamming_distance

---
+++ src/hamming_distance/mod.rs
@@ -0,0 +1,8 @@
+use crate::base::Solution;
+
+impl Solution {
+    pub fn hamming_distance(x: i32, y: i32) -> i32 {
+        let mut res = 0;
+        return res;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -48,3 +48,4 @@ pub mod unique_morse_code_words;
 pub mod unique_paths;
 mod univalued_binary_tree;
 pub mod valid_anagram;
+pub mod hamming_distance;
