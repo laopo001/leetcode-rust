a1936ee1d8ae0623d39820d845a67f9de0db6a38
liaodh <liaodh@jiguang.cn>
2019-04-16 19:09:34 +0800

    add maximum_binary_tree_ii

--- src/lib.rs
+++ src/lib.rs
@@ -47,3 +47,4 @@ pub mod unique_morse_code_words;
 pub mod unique_paths;
 mod univalued_binary_tree;
 pub mod valid_anagram;
+pub mod maximum_binary_tree_ii;
---
+++ src/maximum_binary_tree_ii/mod.rs
@@ -0,0 +1,12 @@
+use crate::base::{Solution, TreeNode};
+
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn insert_into_max_tree(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        val: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        return root;
+    }
+}
