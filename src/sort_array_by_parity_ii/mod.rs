9e36c9ebaa238bbcd747b580d1516e5667bef9e2
laopo001 <353272497@qq.com>
2019-04-17 23:22:48 +0800

    fix sort_array_by_parity_ii

--- src/sort_array_by_parity_ii/mod.rs
+++ src/sort_array_by_parity_ii/mod.rs
@@ -2,6 +2,25 @@ use crate::base::Solution;
 
 impl Solution {
     pub fn sort_array_by_parity_ii(a: Vec<i32>) -> Vec<i32> {
-        return a;
+        let mut arr = a;
+        let mut odd: Vec<i32> = vec![];
+        let mut even: Vec<i32> = vec![];
+
+        for i in arr.iter() {
+            if *i % 2 == 0 {
+                even.push(*i);
+            } else {
+                odd.push(*i);
+            }
+        }
+
+        for i in 0..(arr.len()) {
+            if i % 2 == 0 {
+                arr[i] = even.pop().unwrap();
+            } else {
+                arr[i] = odd.pop().unwrap();
+            }
+        }
+        return arr;
     }
 }
