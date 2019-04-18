67c692fa3451d4310029ad770140984550f73f53
laopo001 <353272497@qq.com>
2019-04-18 16:20:55 +0800

    fix count_primes

--- src/count_primes/mod.rs
+++ src/count_primes/mod.rs
@@ -18,55 +18,40 @@ impl Solution {
         }
         return res;
     }
-
+    /// faster than 100.00%
     pub fn count_primes(n: i32) -> i32 {
         if n <= 2 {
             return 0;
         }
         let mut res = 0;
-        let mut arr: Vec<bool> = vec![true; (n + 1) as usize];
+        let mut arr: Vec<bool> = vec![true; (n) as usize];
         arr[0] = false;
         arr[1] = false;
-        // arr[2]= false;
-        let mut prime = 2;
-        for i in 2..(arr.len()) {
+        'outer: for i in 2..(arr.len()) {
             if !arr[i] {
                 continue;
             }
-            // println!("{:?}1",arr);
-            for j in 2..(arr.len()) {
-                if j != prime && j % prime == 0 {
-                    arr[j] = false;
-                    // println!("{:?}",arr);
+            // res += 1;
+            let mut j = 2;
+            while j * i < (n as usize) {
+                arr[j * i] = false;
+                j += 1;
+            }
+            for k in ((i + 1)..(arr.len())).rev() {
+                if arr[k] {
+                    if k < i * i {
+                        break 'outer;
+                    }
+                    break;
                 }
             }
-            let z = (|x| {
-                for j in ((x + 1)..(arr.len())).rev() {
-                    if arr[j] {
-                        return j;
-                    }
-                }
-                panic!("error");
-                // return 0;
-            })(i);
-            if z < prime * prime {
-                break;
-            }
-            prime = (|x| {
-                for j in (x + 1)..(arr.len()) {
-                    if arr[j] {
-                        return j;
-                    }
-                }
-                panic!("error");
-                // return 0;
-            })(i);
         }
         for i in 0..(arr.len()) {
-            if arr[i] && i != n as usize {
+            if arr[i] {
                 res += 1;
             }
         }
         return res;
     }
+
 }
