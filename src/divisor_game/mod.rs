d85f9f4306d165d457cc6909fd60d14a4e81d07a
laopo001 <353272497@qq.com>
2019-04-21 23:00:52 +0800

    divisor_game 解法2

--- src/divisor_game/mod.rs
+++ src/divisor_game/mod.rs
@@ -8,4 +8,24 @@ impl Solution {
             return false;
         }
     }
+    pub fn divisor_game2(n: i32) -> bool {
+        let mut arr: Vec<i32> = vec![-1; n as usize + 1];
+        arr[1] = 0;
+        for i in 2..=n {
+            for j in 1..i {
+                // 0ms
+                if arr[i as usize - j as usize] == 0 && i % j == 0
+                // 4ms 判断整除计算消耗大于在数组中对比一个项
+                // if i % j == 0 && arr[i as usize - j as usize] == 0
+                {
+                    // 4ms
+                    arr[i as usize] = 1;
+                    break;
+                }
+                arr[i as usize] = 0;
+            }
+        }
+        // println!("{:?}",arr);
+        return arr[n as usize] == 1;
+    }
 }
