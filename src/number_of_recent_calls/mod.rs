91e7985a97a713fda5265bf640519df1141eb173
laopo001 <353272497@qq.com>
2019-05-01 21:33:45 +0800

    add number_of_recent_calls

---
+++ src/number_of_recent_calls/mod.rs
@@ -0,0 +1,16 @@
+struct RecentCounter {
+
+}
+
+
+impl RecentCounter {
+
+    fn new() -> Self {
+        return RecentCounter {};
+    }
+    
+    fn ping(&self, t: i32) -> i32 {
+        return 0;
+    }
+}
+
