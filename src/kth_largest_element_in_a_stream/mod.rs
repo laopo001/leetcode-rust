ecd3e69468d8db442c453d0f6d4db415be3b3268
laopo001 <353272497@qq.com>
2019-05-12 23:07:16 +0800

    fix

--- src/kth_largest_element_in_a_stream/mod.rs
+++ src/kth_largest_element_in_a_stream/mod.rs
@@ -1,10 +1,10 @@
 use std::collections::BinaryHeap;
 
 struct KthLargest {
-    arr:Vec<i32>,
+    arr: Vec<i32>,
 }
 
-fn addBinaryHeap(arr:&mut Vec<i32>,val:i32) {
+fn addBinaryHeap(arr: &mut Vec<i32>, val: i32) {
     if *arr.first().unwrap() < val {
         arr.push(val);
         arr.sort();
@@ -15,16 +15,14 @@ fn addBinaryHeap(arr:&mut Vec<i32>,val:i32) {
 impl KthLargest {
 
     fn new(k: i32, nums: Vec<i32>) -> Self {
-        let mut arr= vec![0;k as usize];
+        let mut arr = vec![i32::min_value(); k as usize];
 
-        for  item  in nums.into_iter(){
+        for item in nums.into_iter() {
             addBinaryHeap(&mut arr, item);
         }
-        KthLargest {
-            arr,
-        }
+        KthLargest { arr }
     }
-    
+
     fn add(&mut self, val: i32) -> i32 {
         addBinaryHeap(&mut self.arr, val);
         *self.arr.first().unwrap()
