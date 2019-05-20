577dd383d6f54f54a94f344a23344be4a14965c0
laopo001 <353272497@qq.com>
2019-05-20 19:10:44 +0800

    fix arranging_coins

--- src/arranging_coins/mod.rs
+++ src/arranging_coins/mod.rs
@@ -2,6 +2,11 @@ use crate::base::Solution;
 
 impl Solution {
 	pub fn arrange_coins(n: i32) -> i32 {
-
+		if n == 1 { return 1; }
+		let mut t = (((n as f64) * 2.0).sqrt() - 1.0) as i64;
+		while (n as i64) - t * (t + 1) / 2 > t {
+			t += 1;
+		}
+		return t as i32;
 	}
 }
