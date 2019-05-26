6ee285b88dfb1f3e3b8ecbcdbe8949b07f69db9a
laopo001 <353272497@qq.com>
2019-05-26 21:02:28 +0800

    fix beautiful_arrangement

--- src/beautiful_arrangement/mod.rs
+++ src/beautiful_arrangement/mod.rs
@@ -3,10 +3,22 @@ use crate::base::Solution;
 impl Solution {
 	pub fn count_arrangement(n: i32) -> i32 {
 		let mut res = 0;
-		let mut arr: Vec<i32> = vec![0, n + 1];
-		for i in 1..(n + 1) {
-			arr[i] = i;
-		}
+		let mut arr: Vec<i32> = vec![0; n as usize + 1];
+		// println!("{:?}",arr);
+		Solution::helper(n as usize, &mut arr, 1, &mut res);
 		res
+	}
+	fn helper(n: usize, arr: &mut Vec<i32>, pos: usize, res: &mut i32) {
+		if pos > n {
+			*res += 1;
+			return;
+		}
+		for i in 1..(n + 1) {
+			if arr[i] == 0 && (i % pos == 0 || pos % i == 0) {
+				arr[i] = 1;
+				Solution::helper(n, arr, pos + 1, res);
+				arr[i] = 0;
+			}
+		}
 	}
 }
