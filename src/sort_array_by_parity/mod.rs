e6066a900d7072a101b41be312d74ab7f6434176
laopo001 <353272497@qq.com>
2019-05-31 22:48:20 +0800

    fix sort_array_by_parity

--- src/sort_array_by_parity/mod.rs
+++ src/sort_array_by_parity/mod.rs
@@ -2,6 +2,16 @@ use crate::base::Solution;
 
 impl Solution {
 	pub fn sort_array_by_parity(a: Vec<i32>) -> Vec<i32> {
-		a
+		let mut left: Vec<i32> = vec![];
+		let mut right: Vec<i32> = vec![];
+		for item in a.iter() {
+			if *item % 2 == 0 {
+				left.push(*item);
+			} else {
+				right.push(*item);
+			}
+		}
+		left.extend(right);
+		left
 	}
 }
