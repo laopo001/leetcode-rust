688b5107353e1554a0590c8e8a4681af551e0e84
laopo001 <353272497@qq.com>
2019-06-01 23:33:26 +0800

    fix height_checker

--- src/height_checker/mod.rs
+++ src/height_checker/mod.rs
@@ -2,6 +2,14 @@ use crate::base::Solution;
 
 impl Solution {
 	pub fn height_checker(heights: Vec<i32>) -> i32 {
-		0
+		let mut res = 0;
+		let mut arr = heights.clone();
+		arr.sort();
+		for i in 0..heights.len() {
+			if heights[i] != arr[i] {
+				res += 1;
+			}
+		}
+		res
 	}
 }
