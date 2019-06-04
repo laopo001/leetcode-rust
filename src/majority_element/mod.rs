a632f2d210b123a6d2ef19f5446a82a6770ceb06
laopo001 <353272497@qq.com>
2019-06-04 20:46:19 +0800

    fix majority_element

--- src/majority_element/mod.rs
+++ src/majority_element/mod.rs
@@ -1,7 +1,40 @@
 use crate::base::Solution;
+use std::collections::HashMap;
 
 impl Solution {
 	pub fn majority_element(nums: Vec<i32>) -> i32 {
-		0
+		let mut map: HashMap<i32, usize> = HashMap::new();
+		let len = nums.len();
+		for i in nums {
+			if map.contains_key(&i) {
+				let v = map.get_mut(&i).unwrap();
+				*v += 1;
+				if *v > len / 2 {
+					return i;
+				}
+			} else {
+				map.insert(i, 1);
+				if 1 > len / 2 {
+					return i;
+				}
+			}
+		}
+		return 0;
+	}
+	pub fn majority_element2(nums: Vec<i32>) -> i32 {
+		let mut res = nums[0];
+		let mut count = 1;
+		for i in 0..nums.len() {
+			if nums[i] == res {
+				count += 1;
+			} else {
+				count -= 1;
+			}
+			if count == 0 {
+				res = nums[i];
+				count = 1;
+			}
+		}
+		return res;
 	}
 }
