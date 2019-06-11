db7c19ad438048092f26667d2843d9b493d89796
laopo001 <353272497@qq.com>
2019-06-11 22:22:37 +0800

    add ugly-number-ii

--- src/lib.rs
+++ src/lib.rs
@@ -75,3 +75,4 @@ pub mod majority_element;
 pub mod longest_palindromic;
 pub mod leaf_similar_trees;
 pub mod counting_bits;
+pub  mod ugly_number_ii;
---
+++ src/ugly_number_ii/mod.rs
@@ -0,0 +1,7 @@
+use crate::base::Solution;
+
+impl Solution {
+	pub fn nth_ugly_number(n: i32) -> i32 {
+		n
+	}
+}
