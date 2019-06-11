6d66a0dbc39629ae662a951f89aad5acd3b1f7fe
laopo001 <353272497@qq.com>
2019-06-12 01:04:23 +0800

    fix ugly_number_ii

--- src/ugly_number_ii/mod.rs
+++ src/ugly_number_ii/mod.rs
@@ -2,6 +2,29 @@ use crate::base::Solution;
 
 impl Solution {
 	pub fn nth_ugly_number(n: i32) -> i32 {
-		n
+		let n = n as usize;
+		let mut res = vec![1];
+		res[0] = 1;
+		let mut i2 = 0;
+		let mut i3 = 0;
+		let mut i5 = 0;
+		while res.len() < n {
+			let num2 = res[i2] * 2;
+			let num3 = res[i3] * 3;
+			let num5 = res[i5] * 5;
+			let item = std::cmp::min(num2, std::cmp::min(num3, num5));
+//			println!("{}",item);
+			if num2 == item {
+				i2 += 1;
+			}
+			if num3 == item {
+				i3 += 1;
+			}
+			if num5 == item {
+				i5 += 1;
+			}
+			res.push(item);
+		}
+		res[n - 1]
 	}
 }
