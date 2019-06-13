78941077c780e8c85a5a176dcae7a384df38cac4
laopo001 <353272497@qq.com>
2019-06-14 01:10:47 +0800

    fix coin_change

--- src/coin_change/mod.rs
+++ src/coin_change/mod.rs
@@ -1,22 +1,26 @@
+use crate::base::Solution;
+
 impl Solution {
 	pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
-		let arr = vec![0; coins.len()];
-		let mut res = 0;
-		let mut it = 0;
-		while it < coins.len() {
-			if res < amount {
-				it = std::cmp::max(0, it - 1);
-				for i in (0..arr.len()).rev() {
-					res += arr[i] * coins[i];
-					if it == i {
-						arr[i] += 1;
-					}
+		let len = coins.len();
+		let mut dp = vec![vec![std::i32::MAX; amount as usize + 1]; len + 1];
+		for i in 1..(len + 1) {
+			dp[i][0]=0;
+			for j in 1..(amount as usize + 1) {
+				if j >= coins[i - 1] as usize {
+					// println!(dp[i - 1][j]);
+					dp[i][j] = std::cmp::min(dp[i - 1][j] as i64, dp[i][j - coins[i - 1] as usize] as i64 + 1) as i32;
+				} else {
+					dp[i][j] = dp[i - 1][j];
 				}
-			} else {
-				it += 1;
-				arr[it] += 1;
 			}
 		}
-		-1
+		// println!("{:?}",dp);
+		if std::i32::MAX == dp[len][amount as usize] {
+			-1
+		} else {
+			dp[len][amount as usize]
+		}
 	}
 }
+
