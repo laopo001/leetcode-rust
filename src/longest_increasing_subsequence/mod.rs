635b3f6089d74e3fab474610590dcce4cd46c774
laopo001 <353272497@qq.com>
2019-06-22 23:56:18 +0800

    fix longest_increasing_subsequence

--- src/longest_increasing_subsequence/mod.rs
+++ src/longest_increasing_subsequence/mod.rs
@@ -1,7 +1,20 @@
 use crate::base::Solution;
 
 impl Solution {
+	// dp
 	pub fn length_of_lis(nums: Vec<i32>) -> i32 {
-		0
+		use std::cmp::max;
+		let len = nums.len();
+		let mut dp = vec![1; len];
+		let mut res = 0;
+		for i in 0..len {
+			for j in 0..i {
+				if nums[j] < nums[i] {
+					dp[i] = max(dp[j] + 1, dp[i]);
+				}
+			}
+			res = max(dp[i], res);
+		}
+		res
 	}
 }
