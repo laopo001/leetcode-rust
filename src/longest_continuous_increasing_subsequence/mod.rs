9c29db636efc3bf96fdbb053c78be2d7b1b347ac
laopo001 <353272497@qq.com>
2019-06-23 22:34:37 +0800

    fix longest_continuous_increasing_subsequence

--- src/longest_continuous_increasing_subsequence/mod.rs
+++ src/longest_continuous_increasing_subsequence/mod.rs
@@ -2,6 +2,19 @@ use crate::base::Solution;
 
 impl Solution {
 	pub fn find_length_of_lcis(nums: Vec<i32>) -> i32 {
-		0
+		use std::cmp::max;
+		let len = nums.len();
+		if len == 0 {
+			return 0;
+		}
+		let mut dp = vec![1; len];
+		let mut res = 1;
+		for i in 1..len {
+			if nums[i] > nums[i - 1] {
+				dp[i] = dp[i - 1] + 1;
+				res = max(dp[i], res);
+			}
+		}
+		res
 	}
 }
