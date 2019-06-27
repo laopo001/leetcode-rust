8f6e0e29a5d2c520925bced3e93b0b7296a76302
laopo001 <353272497@qq.com>
2019-06-27 19:05:18 +0800

    fix

--- src/longest_arithmetic_sequence/mod.rs
+++ src/longest_arithmetic_sequence/mod.rs
@@ -4,16 +4,16 @@ impl Solution {
 	pub fn longest_arith_seq_length(a: Vec<i32>) -> i32 {
 		use std::collections::{HashMap, HashSet};
 		use std::cmp::max;
-		let mut map: HashMap<i32, bool> = HashMap::new();
-		let mut set: HashSet<i32> = a.into_iter().collect();
-		let mut a: Vec<i32> = set.into_iter().collect();
+		// let mut map: HashMap<i32, bool> = HashMap::new();
+		// let mut set: HashSet<i32> = a.into_iter().collect();
+		// let mut a: Vec<i32> = set.into_iter().collect();
 		let len = a.len();
-		a.sort();
+		// a.sort();
 		let mut dp: Vec<HashMap<i32, usize>> = vec![HashMap::new(); len];
 		let mut res = 0;
 		for i in 0..len {
 			for j in 0..i {
-				let t = (a[i] - a[j]).abs();
+				let t = a[i] - a[j];
 				if !dp[i].contains_key(&t) {
 					dp[i].insert(t, 1);
 				}
@@ -21,13 +21,13 @@ impl Solution {
 				unsafe {
 					let v = std::mem::transmute::<*const _, *mut usize>(value);
 					let value2 = dp[j].get(&t).unwrap_or(&1);
-					*v += *value2;
+					*v = *value2 + 1;
 					res = max(res, *v as i32);
 				}
 			}
 		}
-		println!("{:?}", a);
-		println!("{:?}", dp);
+		// println!("{:?}", a);
+		// println!("{:?}", dp);
 		res
 	}
 }
--- src/longest_palindromic_substring/mod.rs
+++ src/longest_palindromic_substring/mod.rs
@@ -2,74 +2,74 @@ use crate::base::Solution;
 
 #[allow(dead_code)]
 fn cmp(a: &str) -> bool {
-    let len = a.len() / 2;
-    for i in 0..(len) {
-        if a.chars().nth(i) != a.chars().nth(a.len() - 1 - i) {
-            return false;
-        }
-    }
-    return true;
+	let len = a.len() / 2;
+	for i in 0..(len) {
+		if a.chars().nth(i) != a.chars().nth(a.len() - 1 - i) {
+			return false;
+		}
+	}
+	return true;
 }
 
 impl Solution {
-    pub fn longest_palindrome(s: String) -> String {
-        let len = s.len();
-        let mut max_len = "";
-        for i in 1..len {
-            for j in 0..(i + 1) {
-                if j > i / 2 {
-                    continue;
-                }
-                let z = s.get(j..(i + 1)).unwrap();
-                if cmp(z) {
-                    if max_len.len() < z.len() {
-                        max_len = z;
-                    }
-                }
-            }
-        }
-        return max_len.to_string();
-    }
-    //
-    // dp[i, j]   = 1                                 if i == j
+	pub fn longest_palindrome(s: String) -> String {
+		let len = s.len();
+		let mut max_len = "";
+		for i in 1..len {
+			for j in 0..(i + 1) {
+				if j > i / 2 {
+					continue;
+				}
+				let z = s.get(j..(i + 1)).unwrap();
+				if cmp(z) {
+					if max_len.len() < z.len() {
+						max_len = z;
+					}
+				}
+			}
+		}
+		return max_len.to_string();
+	}
+	//
+	// dp[i, j]   = 1                                 if i == j
 
-    //           = s[] == s[j]                        if j = i + 1
+	//           = s[] == s[j]                        if j = i + 1
 
-    //           = s[i] == s[j] && dp[i + 1][j - 1]   if j > i + 1
-    #[warn(deprecated)]
-    pub fn longest_palindrome2(s: String) -> String {
-        let mut dp: Vec<Vec<i32>> = vec![vec![0; s.len()]; s.len()];
-        let c = s.as_bytes();
-        let len = s.len();
-        let mut max_len = 0;
-        let mut max_index = 0;
-        for i in 0..len {
-            dp[i][i] = 1;
-            max_len = 1;
-            max_index = i;
-        }
-        for i in 0..len {
-            let j = i + 1;
-            if j < len && c[i] == c[j] {
-                dp[i][j] = 1;
-                max_len = 2;
-                max_index = i;
-            }
-        }
-        for i in 0..len {
-            for j in 0..i {
-                if i > j + 1 && c[i] == c[j] && dp[j + 1][i - 1] == 1 {
-                    dp[j][i] = 1;
-                    // println!("{:?},{:?}", j, i);
-                    if i - j + 1 > max_len {
-                        max_len = i - j + 1;
-                        max_index = j;
-                    }
-                }
-            }
-        }
-        unsafe {
-            return s.get_unchecked(max_index..max_index + max_len).to_string();
-        }
-    }
+	//           = s[i] == s[j] && dp[i + 1][j - 1]   if j > i + 1
+	#[warn(deprecated)]
+	pub fn longest_palindrome2(s: String) -> String {
+		let mut dp: Vec<Vec<i32>> = vec![vec![0; s.len()]; s.len()];
+		let c = s.as_bytes();
+		let len = s.len();
+		let mut max_len = 0;
+		let mut max_index = 0;
+		for i in 0..len {
+			dp[i][i] = 1;
+			max_len = 1;
+			max_index = i;
+		}
+		for i in 0..len {
+			let j = i + 1;
+			if j < len && c[i] == c[j] {
+				dp[i][j] = 1;
+				max_len = 2;
+				max_index = i;
+			}
+		}
+		for i in 0..len {
+			for j in 0..i {
+				if i > j + 1 && c[i] == c[j] && dp[j + 1][i - 1] == 1 {
+					dp[j][i] = 1;
+					// println!("{:?},{:?}", j, i);
+					if i - j + 1 > max_len {
+						max_len = i - j + 1;
+						max_index = j;
+					}
+				}
+			}
+		}
+		unsafe {
+			return s.get_unchecked(max_index..max_index + max_len).to_string();
+		}
+	}
 }
