b815b53f1e2c8d57f2358ad6da2a09ea3ba1ca90
laopo001 <353272497@qq.com>
2019-06-27 23:47:30 +0800

    fix n-ary-tree-postorder-traversal

--- src/n-ary-tree-postorder-traversal.ts
+++ src/n-ary-tree-postorder-traversal.ts
@@ -10,5 +10,16 @@
  * @return {number[]}
  */
 var postorder = function (root) {
+    let arr = [];
+    if (root == null) return arr;
+    function run(root) {
+        for (let i = 0; i < root.children.length; i++) {
+            const item = root.children[i];
+            run(item);
 
+        }
+        arr.push(root.val)
+    }
+    run(root);
+    return arr;
 };
