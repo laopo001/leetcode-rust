8a8ad20cfbb776e135f3e2c2ccd82be4a498316f
laopo001 <353272497@qq.com>
2019-07-04 00:00:26 +0800

    longest_increasing_subsequence

--- src/longest_increasing_subsequence/index.ts
+++ src/longest_increasing_subsequence/index.ts
@@ -3,26 +3,26 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
-    let res = -Infinity;
+    let res = 0;
+    if (nums.length === 0) {
+        return 0;
+    }
+    if (nums.length === 1) {
+        return 1;
+    }
     let map = {};
     function run(nums) {
         let len = nums.length;
         if (map[len] != null) {
             return map[len]
         }
-        if (len === 0) {
-            return 0;
-        }
-        if (len === 1) {
-            return 1;
-        }
         let max = 1;
         for (let i = 0; i < len - 1; i++) {
             if (nums[i] < nums[len - 1]) {
                 max = Math.max(max, run(nums.slice(0, i + 1)) + 1)
-                res = Math.max(res, max)
             }
         }
+        res = Math.max(res, max)
         if (map[len] == null) {
             map[len] = max
         }
