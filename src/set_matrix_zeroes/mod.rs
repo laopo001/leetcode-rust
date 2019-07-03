6272a4184a7ff6030f2b03a87b77f224c7bd8ae4
laopo001 <353272497@qq.com>
2019-07-03 11:24:09 +0800

    fix set_matrix_zeroes

--- src/set_matrix_zeroes/mod.rs
+++ src/set_matrix_zeroes/mod.rs
@@ -1,5 +1,30 @@
 use crate::base::Solution;
 
 impl Solution {
-    pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {}
+    pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
+        let m = matrix.len();
+        if m == 0 {
+            return;
+        }
+        let n = matrix[0].len();
+        let mut copy = vec![vec![1; n]; m];
+        for i in 0..m {
+            for j in 0..n {
+                if matrix[i][j] == 0 {
+                    for i in 0..m {
+                        copy[i][j] = 0;
+                    }
+                    for j in 0..n {
+                        copy[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        for i in 0..m {
+            for j in 0..n {
+                matrix[i][j] = matrix[i][j] * copy[i][j];
+            }
+        }
+    }
 }
