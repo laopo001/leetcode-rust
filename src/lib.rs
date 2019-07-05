ec7afba3f9671358b5ee2df6657ed2ffbcf03e35
laopo001 <353272497@qq.com>
2019-07-05 23:03:44 +0800

    add subtree_of_another_tree

--- src/lib.rs
+++ src/lib.rs
@@ -77,6 +77,7 @@ pub mod sort_array_by_parity_ii;
 pub mod sort_colors;
 pub mod squares_of_a_sorted_array;
 pub mod stone_game;
+pub mod subtree_of_another_tree;
 pub mod sum_of_two_integers;
 mod sum_root_to_leaf_numbers;
 mod surface_area_of_3d_shapes;
---
+++ src/subtree_of_another_tree/mod.rs
@@ -0,0 +1,8 @@
+use crate::base::{Solution,TreeNode};
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn is_subtree(s: Option<Rc<RefCell<TreeNode>>>, t: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        true
+    }
+}
