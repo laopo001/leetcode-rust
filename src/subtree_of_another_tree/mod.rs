8de1550deb45d27693991bdf4641ff858324c907
laopo001 <353272497@qq.com>
2019-07-07 18:46:52 +0800

    fix subtree_of_another_tree

--- src/subtree_of_another_tree/mod.rs
+++ src/subtree_of_another_tree/mod.rs
@@ -1,8 +1,50 @@
-use crate::base::{Solution,TreeNode};
-use std::rc::Rc;
+use crate::base::{Solution, TreeNode};
 use std::cell::RefCell;
+use std::rc::Rc;
+
+fn run(s: Option<Rc<RefCell<TreeNode>>>, t: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    if s.is_none() && t.is_none() {
+        return true;
+    } else if s.is_some() && t.is_some() {
+        let s_s = s.unwrap();
+        if s_s.borrow().val == t.clone().unwrap().borrow().val {
+            return Solution::is_subtree(
+                s_s.borrow().left.clone(),
+                t.clone().unwrap().borrow().left.clone(),
+            ) && Solution::is_subtree(
+                s_s.borrow().right.clone(),
+                t.clone().unwrap().borrow().right.clone(),
+            );
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
 impl Solution {
+    // 4ms 100%
     pub fn is_subtree(s: Option<Rc<RefCell<TreeNode>>>, t: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        true
+        if s.is_none() && t.is_none() {
+            return true;
+        } else if s.is_some() && t.is_some() {
+            let s_s = s.unwrap();
+            if s_s.borrow().val == t.clone().unwrap().borrow().val {
+                return (run(
+                    s_s.borrow().left.clone(),
+                    t.clone().unwrap().borrow().left.clone(),
+                ) && run(
+                    s_s.borrow().right.clone(),
+                    t.clone().unwrap().borrow().right.clone(),
+                )) || Solution::is_subtree(s_s.borrow().left.clone(), t.clone())
+                    || Solution::is_subtree(s_s.borrow().right.clone(), t);
+            } else {
+                return Solution::is_subtree(s_s.borrow().left.clone(), t.clone())
+                    || Solution::is_subtree(s_s.borrow().right.clone(), t);
+            }
+        } else {
+            return false;
+        }
     }
 }
