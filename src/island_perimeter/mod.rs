39ebd73671503225039d5426586d30718f90678b
laopo001 <353272497@qq.com>
2019-07-11 00:38:43 +0800

    fix island_perimeter

--- src/island_perimeter/mod.rs
+++ src/island_perimeter/mod.rs
@@ -2,18 +2,22 @@ use crate::base::Solution;
 
 fn get_one_nums(grid: &Vec<Vec<i32>>, i: usize, j: usize) -> i32 {
 	let mut res = 0;
-	let arr: Vec<i32> = vec![];
-	if grid[i - 1][j] == 1 {
-		res + 1;
+	let x_len = grid.len();
+    if x_len == 0 {
+        return res;
+    }
+    let y_len = grid[0].len();
+	if i!=0 && grid[i - 1][j] == 1 {
+		res += 1;
 	}
-	if grid[i + 1][j] == 1 {
-		res + 1;
+	if i !=x_len -1 && grid[i + 1][j] == 1 {
+		res += 1;
 	}
-	if grid[i][j - 1] == 1 {
-		res + 1;
+	if j!=0 && grid[i][j - 1] == 1 {
+		res += 1;
 	}
-	if grid[i][j + 1] == 1 {
-		res + 1;
+	if j !=y_len -1 && grid[i][j + 1] == 1 {
+		res += 1;
 	}
 	res
 }
