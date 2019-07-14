826f9119ab2e6d03c01122966df706832f96d359
laopo001 <353272497@qq.com>
2019-07-15 01:04:18 +0800

    add remove_duplicates_from_sorted_list

--- src/remove_duplicates_from_sorted_list/mod.rs
+++ src/remove_duplicates_from_sorted_list/mod.rs
@@ -1,7 +1,36 @@
 use crate::base::{Solution, ListNode};
 
 impl Solution {
+	// 0 ms, faster than 100.00%
 	pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+		let mut arr: Vec<i32> = vec![];
+		let mut head = head;
+		while let Some(node) = head {
+			arr.push(node.val);
+			head = node.next;
+		}
+		let mut res = None;
+		let mut p: Option<i32> = None;
+		while arr.len() != 0 {
+			let mut q = arr.pop().unwrap();
+			if p.is_none() {
+				p = Some(q);
+			} else {
+				unsafe {
+					if p.unwrap() == q {
+						continue;
+					} else {
+						p = Some(q);
+					}
+				}
+			}
+			let mut node = ListNode::new(q);
+			node.next = res;
+			res = Some(Box::new(node));
+		}
+		res
+	}
+	pub fn delete_duplicates2(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
 		let mut root: *mut Box<ListNode> = std::ptr::null_mut();
 		let mut node = head.clone();
 		loop {
