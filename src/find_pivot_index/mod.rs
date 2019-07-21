8b8a297fcc22a393f6a4e17a584a6d90eb1b16f3
laopo001 <353272497@qq.com>
2019-07-21 16:14:11 +0800

    fix find_pivot_index

--- src/find_pivot_index/mod.rs
+++ src/find_pivot_index/mod.rs
@@ -1,7 +1,6 @@
 use crate::base::Solution;
 use std::collections::HashMap;
 
-
 fn sum_nums(l: usize, r: usize, nums: &Vec<i32>, map: &mut Vec<Vec<Option<i32>>>) -> i32 {
 	if map[l][r].is_some() {
 		return map[l][r].unwrap();
@@ -13,7 +12,7 @@ fn sum_nums(l: usize, r: usize, nums: &Vec<i32>, map: &mut Vec<Vec<Option<i32>>>
 	if l + 1 == r {
 		res = nums[l];
 	} else {
-		res = nums[l] + sum_nums(l + 1, r, nums, map);
+		res = nums[r - 1] + sum_nums(l, r - 1, nums, map);
 	}
 	map[l][r] = Some(res);
 	return res;
@@ -24,13 +23,19 @@ impl Solution {
 	pub fn pivot_index(nums: Vec<i32>) -> i32 {
 		let len = nums.len();
 		let mut res = -1;
-//		let mut map: HashMap<(usize, usize), i32> = HashMap::new();
-		let mut map: Vec<Vec<Option<i32>>> = vec![vec![None; len]; len];
+		let mut sum = 0;
+		for num in nums.iter() {
+			sum += *num;
+		}
+		// let mut map: Vec<Vec<Option<i32>>> = vec![vec![None; len]; len]; // 这题有内存限制,	Memory Limit Exceeded
+		let mut q = 0;
 		for i in 0..len {
-			if sum_nums(0, i, &nums, &mut map) == sum_nums(i + 1, len, &nums, &mut map) {
+			let temp = q;
+			if temp == sum - temp - nums[i] {
 				res = i as i32;
 				break;
 			}
+			q += nums[i];
 		}
 		res
 	}
