0bc83b78a47f95e16f0860a00640872cea1bf29f
laopo001 <353272497@qq.com>
2019-07-23 23:56:08 +0800

    fix

--- src/spiral_matrix/mod.rs
+++ src/spiral_matrix/mod.rs
@@ -2,8 +2,14 @@ use crate::base::Solution;
 
 impl Solution {
 	pub fn spiral_order(matrix: Vec<Vec<i32>>) -> Vec<i32> {
-		let arr_x = [1, -1, -1, 1];
-		let arr_x = [1, -1, -1, 1];
+		let arr_x = [1, 0, -1, 0];
+		let arr_y = [0, 1, 0, -1];
+		let len_y = matrix.len();
+		if len_y == 0{
+			return vec![];
+		}
+		let len_x= matrix[0].len();
+
 		vec![0]
 	}
 }
