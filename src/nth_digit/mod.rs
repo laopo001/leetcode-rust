e828aec03811e0fb6b76debd0cc0c6dfdae23540
laopo001 <353272497@qq.com>
2019-07-31 22:42:41 +0800

    fix nth_digit

--- src/nth_digit/mod.rs
+++ src/nth_digit/mod.rs
@@ -2,6 +2,25 @@ use crate::base::Solution;
 
 impl Solution {
 	pub fn find_nth_digit(n: i32) -> i32 {
-		0
+		// if n <= 9 {
+		// 	return n;
+		// }
+		let mut n = n as u32;
+		let mut i = 0;
+		let mut j = 1;
+		let mut q = 0;
+		while n > 9 * 10_u32.pow(i) * j {
+			q += 9 * 10_u32.pow(i);
+			n -= 9 * 10_u32.pow(i) * j;
+			i += 1;
+			j += 1;
+		}
+		// println!("{},{},{}", q, j, n);
+		let z = ((n as f32) / (j as f32)).ceil() as u32;
+		let p = q + z;
+		let nth = ((n - (z - 1) * j) - 1) as usize;
+		let s = p.to_string();
+		// println!("{},{},{}", p, nth, s);
+		return s.as_bytes()[nth] as i32 - 48;
 	}
 }
