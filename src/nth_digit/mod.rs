e4ee28fec17daadec228fa84558ec602baa1cffc
laopo001 <353272497@qq.com>
2019-07-31 22:48:43 +0800

    fix

--- src/nth_digit/mod.rs
+++ src/nth_digit/mod.rs
@@ -1,6 +1,7 @@
 use crate::base::Solution;
 
 impl Solution {
+	// 0 ms, faster than 100.00%
 	pub fn find_nth_digit(n: i32) -> i32 {
 		// if n <= 9 {
 		// 	return n;
@@ -18,7 +19,8 @@ impl Solution {
 		// println!("{},{},{}", q, j, n);
 		let z = ((n as f32) / (j as f32)).ceil() as u32;
 		let p = q + z;
-		let nth = ((n - (z - 1) * j) - 1) as usize;
+		// let nth = ((n-(z-1)*j) - 1) as usize;
+		let nth = ((n-1) % j) as usize;
 		let s = p.to_string();
 		// println!("{},{},{}", p, nth, s);
 		return s.as_bytes()[nth] as i32 - 48;
