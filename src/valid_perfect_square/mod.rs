654ecdd5e9b16b4f2211223491ccb7308d3a055f
laopo001 <353272497@qq.com>
2019-08-02 10:46:30 +0800

    valid_perfect_square

--- src/valid_perfect_square/mod.rs
+++ src/valid_perfect_square/mod.rs
@@ -1,7 +1,20 @@
 use crate::base::Solution;
 
 impl Solution {
+	// 0 ms, faster than 100.00%
 	pub fn is_perfect_square(num: i32) -> bool {
-		return  true;
+		let mut i: i64 = 0;
+		let mut res: i64 = i * i;
+		if 0 == num {
+			return false;
+		}
+		while res < num as i64 {
+			i += 1;
+			res = i * i;
+			if res == num as i64 {
+				return true;
+			}
+		}
+		return false;
 	}
 }
