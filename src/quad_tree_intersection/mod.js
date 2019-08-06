88af1f5b9c553e770095e744bf9b52bedbf0b408
laopo001 <353272497@qq.com>
2019-08-06 20:50:38 +0800

    fix quad_tree_intersection

--- src/quad_tree_intersection/mod.js
+++ src/quad_tree_intersection/mod.js
@@ -9,11 +9,27 @@
  *    this.bottomRight = bottomRight;
  * };
  */
+function run(quadTree1, quadTree2) {
+    run(quadTree1.topLeft, quadTree2.topLeft);
+    run(quadTree1.topRight, quadTree2.topRight);
+    run(quadTree1.bottomLeft, quadTree2.bottomLeft);
+    run(quadTree1.bottomRight, quadTree2.bottomRight);
+    if (quadTree1 != null && quadTree1.val) {
+        return quadTree1;
+    }
+    return quadTree2;
+}
+
 /**
  * @param {Node} quadTree1
  * @param {Node} quadTree2
  * @return {Node}
  */
-var intersect = function(quadTree1, quadTree2) {
-
-};s
+var intersect = function (quadTree1, quadTree2) {
+    return new Node(quadTree1.val && quadTree2.val, false,
+        run(quadTree1.topLeft, quadTree2.topLeft),
+        run(quadTree1.topRight, quadTree2.topRight),
+        run(quadTree1.bottomLeft, quadTree2.bottomLeft),
+        run(quadTree1.bottomRight, quadTree2.bottomRight),
+    );
+};
