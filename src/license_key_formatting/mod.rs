f401a8df6f10b36fac1d4a617de86ca9901f604a
laopo001 <353272497@qq.com>
2019-08-15 22:54:45 +0800

    improve license_key_formatting

--- src/license_key_formatting/mod.rs
+++ src/license_key_formatting/mod.rs
@@ -24,4 +24,34 @@ impl Solution {
 		}
 		res
 	}
+	// 8 ms, faster than 20.00%
+	pub fn license_key_formatting2(s: String, k: i32) -> String {
+		let split = s.split("-");
+		let vec: Vec<&str> = split.collect();
+		let mut r = "".to_string();
+		for item in vec {
+			r += item;
+		}
+		let mut i = 0;
+		let mut res = "".to_string();
+		for item in r.chars().rev() {
+			i += 1;
+			res = res + item.to_string().to_ascii_uppercase().as_str();
+			if k == i {
+				res = res + "-";
+				i = 0;
+			}
+		}
+		let start = res.len() - 1;
+		let end = res.len();
+		if res.get(start..end).is_some() && res.get(start..end).unwrap() == "-" {
+			res = res.get(0..start).unwrap_or("").to_string();
+		}
+		unsafe {
+			let temp = res.as_mut_vec();
+			temp.reverse();
+		}
+
+		res
+	}
 }
