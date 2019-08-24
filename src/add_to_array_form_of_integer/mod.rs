5556861411ee2bc83950e35a6ee4b4054f6a6149
laopo001 <353272497@qq.com>
2019-08-25 00:15:19 +0800

    fix add_to_array_form_of_integer

--- src/add_to_array_form_of_integer/mod.rs
+++ src/add_to_array_form_of_integer/mod.rs
@@ -1,6 +1,7 @@
 use crate::base::Solution;
 
 impl Solution {
+	// 12 ms, faster than 100.00%
 	pub fn add_to_array_form(a: Vec<i32>, k: i32) -> Vec<i32> {
 		let mut b: Vec<i32> = vec![];
 		let mut t = k;
@@ -9,13 +10,30 @@ impl Solution {
 			t /= 10;
 		}
 		b.push(t);
-//		println!("{:?}", arr);
 		let mut a = a;
 		a.reverse();
-		let len = if a.len() > b.len() { a.len() } else { b.len() };
-		for i in 0..len{
-			
+		let len = if a.len() > b.len() { a.len() } else {
+			std::mem::swap(&mut a,&mut b);
+			a.len()
+		};
+		let mut t = 0;
+		for i in 0..len {
+			let p = *a.get(i).unwrap_or(&0);
+			let q = *b.get(i).unwrap_or(&0);
+			let mut res = p + q + t;
+			// println!("{:?}", res);
+			if res >= 10 {
+				res = res - 10;
+				t = 1;
+			} else {
+				t = 0;
+			}
+			a[i] = res;
 		}
-		vec![0]
+		if t == 1 {
+			a.push(t);
+		}
+		a.reverse();
+		a
 	}
 }
