0641435ddcd140bfbf312d94d7f8143b7198b395
laopo001 <353272497@qq.com>
2019-08-24 23:56:01 +0800

    fix

--- src/add_to_array_form_of_integer/mod.rs
+++ src/add_to_array_form_of_integer/mod.rs
@@ -2,6 +2,13 @@ use crate::base::Solution;
 
 impl Solution {
 	pub fn add_to_array_form(a: Vec<i32>, k: i32) -> Vec<i32> {
+		let mut arr: Vec<i32> = vec![];
+		let mut t = k;
+		while t / 10 != 0 {
+			arr.push(k % 10);
+			t /= 10;
+		}
+		println!("{:?}", arr);
 		vec![0]
 	}
 }
