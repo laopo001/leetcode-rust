181a1289bfe15c2e59f9e03dc2fb943c0929c0c2
laopo001 <353272497@qq.com>
2019-09-01 23:41:07 +0800

    fix permutations_ii

--- src/permutations_ii/mod.rs
+++ src/permutations_ii/mod.rs
@@ -4,7 +4,17 @@ use std::collections::HashMap;
 fn run(nums: Vec<i32>, num: i32) -> Vec<Vec<i32>> {
     let mut res: Vec<Vec<i32>> = vec![];
     let slice = nums.as_slice();
+    let mut t:Option<i32> = None;
     for i in 0..slice.len() {
+        if t.is_some() {
+            if  t.unwrap() != slice[i] {
+                t = Some(slice[i]);
+            }else{
+                continue;
+            }
+        }else{
+            t = Some(slice[i]);
+        }
         let (a, b) = slice.split_at(i);
         let mut temp = a.to_vec();
         temp.extend_from_slice(&[num]);
@@ -24,6 +34,8 @@ impl Solution {
         if nums.len() == 1 {
             return vec![vec![nums[0]]];
         }
+        let mut nums=nums;
+        nums.sort();
         let mut res: Vec<Vec<i32>> = vec![vec![nums[0]]];
         for i in 1..nums.len() {
             let mut temp: Vec<Vec<i32>> = vec![];
