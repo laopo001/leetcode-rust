2078f39e8514377adfe3b376e8d5d25ed136fbad
laopo001 <353272497@qq.com>
2019-09-02 01:21:59 +0800

    fix

--- src/permutations_ii/mod.rs
+++ src/permutations_ii/mod.rs
@@ -2,18 +2,22 @@ use crate::base::Solution;
 use std::collections::HashMap;
 
 fn run(nums: Vec<i32>, num: i32) -> Vec<Vec<i32>> {
+    println!("{:?}",nums);
     let mut res: Vec<Vec<i32>> = vec![];
     let slice = nums.as_slice();
     let mut t:Option<i32> = None;
     for i in 0..slice.len() {
         if t.is_some() {
-            if  t.unwrap() != slice[i] {
-                t = Some(slice[i]);
-            }else{
+            if  t.unwrap() == slice[i] {
                 continue;
+            }else{
+                t= None;
             }
         }else{
-            t = Some(slice[i]);
+            if slice[i] == num {
+                t = Some(slice[i]);
+            }
+
         }
         let (a, b) = slice.split_at(i);
         let mut temp = a.to_vec();
@@ -21,15 +25,16 @@ fn run(nums: Vec<i32>, num: i32) -> Vec<Vec<i32>> {
         temp.extend_from_slice(b);
         res.push(temp);
     }
-    let mut w = nums.clone();
-    w.push(num);
-    res.push(w);
+    if t.is_none() || num != t.unwrap(){
+        let mut w = nums.clone();
+        w.push(num);
+        res.push(w);
+    }
     // println!("{:?}", res);
     return res;
 }
 
 impl Solution {
-    // 132 ms, faster than 7.14%
     pub fn permute_unique(nums: Vec<i32>) -> Vec<Vec<i32>> {
         if nums.len() == 1 {
             return vec![vec![nums[0]]];
@@ -51,7 +56,7 @@ impl Solution {
         let mut map: HashMap<String, bool> = HashMap::new();
         let mut q: Vec<Vec<i32>> = vec![];
         for item in res {
-            let s = item.iter().map(|x| { x.to_string() }).collect::<Vec<String>>().join("-").to_string();
+            let s = item.iter().map(|x| { x.to_string() }).collect::<Vec<String>>().join("").to_string();
             if !map.get(&s).unwrap_or(&false) {
                 map.insert(s, true);
                 q.push(item);
