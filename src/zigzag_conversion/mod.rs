cd8402147eccdb9a680c7aa31fdcfbf393dad29b
laopo001 <353272497@qq.com>
2019-09-22 23:31:19 +0800

    fix zigzag_conversion

--- src/zigzag_conversion/mod.rs
+++ src/zigzag_conversion/mod.rs
@@ -2,6 +2,23 @@ use crate::base::Solution;
 
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
-        "".to_string()
+        let num_rows = num_rows as usize;
+
+        if num_rows < 2 {
+            return s.into();
+        }
+
+        let mut strings = vec![String::new(); num_rows];
+
+        let mut i = 0;
+        let mut down = true;
+
+        for c in s.chars() {
+            strings[i].push(c);
+            i = if down { i + 1 } else { i - 1 };
+            down = down == (i > 0 && i < num_rows - 1);
+        }
+
+        strings.concat()
     }
 }
