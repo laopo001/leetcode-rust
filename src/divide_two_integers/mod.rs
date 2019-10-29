d620f449fe9c1ead35a6e6214498f765b88d3d72
laopo001 <353272497@qq.com>
2019-10-29 20:01:12 +0800

    fix divide_two_integers

--- src/divide_two_integers/mod.rs
+++ src/divide_two_integers/mod.rs
@@ -2,7 +2,18 @@ use crate::base::Solution;
 
 impl Solution {
     pub fn divide(mut dividend: i32, mut divisor: i32) -> i32 {
-        let mut q = 1;
+        if dividend == -2147483648 && divisor == -1 {
+            return 2147483647;
+        }
+        let mut dividend = dividend as i64;
+        let mut divisor = divisor as i64;
+        if divisor == 1 {
+            return dividend as i32;
+        }
+        if divisor == -1 {
+            return -dividend as i32;
+        }
+        let mut q = 1 as i64;
         if dividend < 0 {
             dividend = -dividend;
             q = q * -1;
@@ -26,11 +37,11 @@ impl Solution {
                 break;
             } else {
                 if (i * divisor) == dividend {
-                    return q * i;
+                    return (q * i) as i32;
                 }
                 i = i + 1;
             }
         }
-        q * i
+        (q * i) as i32
     }
 }
