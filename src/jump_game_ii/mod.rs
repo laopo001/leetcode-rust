be6902300e7aa127ede69e150421d488cf8269c1
laopo001 <353272497@qq.com>
2019-12-10 03:39:20 +0800

    fix

--- src/jump_game_ii/mod.rs
+++ src/jump_game_ii/mod.rs
@@ -1,23 +1,30 @@
 #![allow(unused)]
+struct Solution;
 
-fn run(nums: &Vec<i32>, i: usize) -> i32 {
-    if i >= nums.len() {
+fn run(nums: &Vec<i32>, i: usize) -> i64 {
+    // println!("{:?}", i);
+    let len = nums.len();
+    if i >= len - 1 {
         return 0;
     }
+    if nums[i] == 0 {
+        return 1 + run(&nums, i + 1);
+    }
     std::cmp::min(
-        1 + run(&nums, i + nums[i] as usize),
-        2 + run(&nums, i + nums[i + 1] as usize),
+        1 + if i + nums[i] as usize > len - 1 {
+            0
+        } else {
+            run(&nums, i + nums[i] as usize)
+        },
+        1 + run(&nums, i + 1),
     )
 }
-
-struct Solution;
 impl Solution {
     pub fn jump(nums: Vec<i32>) -> i32 {
-        run(&nums, 0)
+        run(&nums, 0) as i32
     }
 }
-
 #[test]
 fn test() {
-    Solution::jump(vec![2, 3, 1, 1, 4]);
+    Solution::jump(vec![4, 1, 1, 3, 1, 1, 1]);
 }
