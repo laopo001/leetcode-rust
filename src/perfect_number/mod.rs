1a9c8b7d42484f778b219c39390319ef42a4d0c1
laopo001 <353272497@qq.com>
2019-12-25 20:42:14 +0800

    fix perfect_number

--- src/perfect_number/mod.rs
+++ src/perfect_number/mod.rs
@@ -1,7 +1,26 @@
 struct Solution;
-
+// Runtime: 0 ms, faster than 100.00%
 impl Solution {
     pub fn check_perfect_number(num: i32) -> bool {
-        false
+        if num <= 1 {
+            return false;
+        }
+        let mut t = 2;
+        let mut arr: Vec<i32> = vec![1];
+        while t * t < num {
+            let res = num % t;
+            if res == 0 {
+                arr.push(t);
+                arr.push(num / t);
+            }
+            t += 1;
+        }
+        // println!("{:?}", arr);
+        arr.into_iter().fold(0, |a, b| a + b) == num
     }
+}
+
+#[test]
+fn test_check_perfect_number() {
+    assert_eq!(Solution::check_perfect_number(28), true);
 }
