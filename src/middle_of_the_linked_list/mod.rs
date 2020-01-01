1abb36b81b106820bbcfd4eefa5928e7925f2f5b
laopo001 <353272497@qq.com>
2020-01-01 22:10:31 +0800

    fix middle_of_the_linked_list

--- src/middle_of_the_linked_list/mod.rs
+++ src/middle_of_the_linked_list/mod.rs
@@ -1,7 +1,15 @@
 use crate::base::ListNode;
 struct Solution;
+// Runtime: 0 ms, faster than 100.00%
 impl Solution {
     pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        head
+        let mut res = vec![];
+        let mut temp = head;
+        while temp.is_some() {
+            res.push(temp.clone());
+            temp = temp.unwrap().next;
+        }
+        let index = res.len() / 2;
+        res[index].clone()
     }
 }
