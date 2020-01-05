a455b505f7e6a28349e3b187ecb9dffa69bfdc6b
laopo001 <353272497@qq.com>
2020-01-05 21:44:49 +0800

    fix minimum_absolute_difference 12ms

--- src/minimum_absolute_difference/mod.rs
+++ src/minimum_absolute_difference/mod.rs
@@ -1,5 +1,6 @@
 struct Solution;
 
+// Runtime: 12 ms, faster than 100.00%
 impl Solution {
     pub fn minimum_abs_difference(mut arr: Vec<i32>) -> Vec<Vec<i32>> {
         if arr.len() <= 1 {
@@ -9,11 +10,12 @@ impl Solution {
         let mut min = std::i32::MAX;
         let mut res: Vec<Vec<i32>> = vec![];
         for i in 0..(arr.len() - 1) {
-            if arr[i + 1] - arr[i] < min {
-                min = arr[i + 1] - arr[i];
+            let temp = arr[i + 1] - arr[i];
+            if temp < min {
+                min = temp;
                 res.clear();
                 res.push(vec![arr[i], arr[i + 1]]);
-            } else if arr[i + 1] - arr[i] == min {
+            } else if temp == min {
                 res.push(vec![arr[i], arr[i + 1]]);
             }
         }
