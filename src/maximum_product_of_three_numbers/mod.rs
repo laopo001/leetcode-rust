180c0e36983751afd0192734a62f85de235ac1c7
laopo001 <353272497@qq.com>
2020-07-30 15:25:50 +0800

    fix maximum_product_of_three_numbers

--- src/maximum_product_of_three_numbers/mod.rs
+++ src/maximum_product_of_three_numbers/mod.rs
@@ -1,6 +1,17 @@
 struct Solution;
 impl Solution {
-    pub fn maximum_product(nums: Vec<i32>) -> i32 {
-        0
+    pub fn maximum_product(mut nums: Vec<i32>) -> i32 {
+        if (nums.len() < 4) {
+            return nums.iter().fold(1, |a, b| a * b);
+        }
+        nums.sort();
+        let a = nums[nums.len() - 1];
+        let q = nums[0] * nums[1];
+        let w = nums[nums.len() - 2] * nums[nums.len() - 3];
+        if (q > w) {
+            return a * q;
+        } else {
+            return a * w;
+        }
     }
 }
