3b9158518707f93223e62f16e0aa9e11435493b9
laopo001 <353272497@qq.com>
2021-05-19 23:28:51 +0800

    fix

--- src/minimum_size_subarray_sum.rs
+++ src/minimum_size_subarray_sum.rs
@@ -1,16 +1,34 @@
+use core::num;
+
 struct Solution;
 
+
 impl Solution {
+    //  0 ms, faster than 100.00%
     pub fn min_sub_array_len(target: i32, mut nums: Vec<i32>) -> i32 {
         let mut sum = 0;
-        let mut z = 0;
-        for i in nums.iter() {
-            sum += *i;
-            z += 1;
-            if(sum >= target){
-                break;
+        let mut res = std::i32::MAX;
+        let mut start = 0;
+        let mut end = 0;
+        let len = nums.len();
+        loop {
+            if (sum >= target) {
+                res = std::cmp::min(res, (end - start) as i32);
+                sum -= nums[start];
+                start += 1;
+            } else {
+                if(end == len){
+                    break;
+                }
+                sum += nums[end];
+                end += 1;
             }
         }
-        z
+        if (res == std::i32::MAX){
+            0
+        } else {
+          res 
+        }
+
     }
 }
