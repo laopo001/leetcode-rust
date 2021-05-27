0396809b51fb05501989c3e884f9bbc6f516b44a
laopo001 <353272497@qq.com>
2021-05-27 15:04:08 +0800

    fix one_bit_and_two_bit_characters

--- src/one_bit_and_two_bit_characters.rs
+++ src/one_bit_and_two_bit_characters.rs
@@ -1,7 +1,33 @@
 struct Solution;
 impl Solution {
+    // 0 ms, faster than 100.00% 
     pub fn is_one_bit_character(bits: Vec<i32>) -> bool {
-        
-        false       
+        if (bits[bits.len() - 1] == 1) {
+            return false;
+        } else {
+            let mut i = 0;
+            while (i < (bits.len() + 1)) {
+                if (i == bits.len()) {
+                    return false;
+                }
+                if (bits[i] == 0) {
+                    if (i == bits.len() - 1) {
+                        break;
+                    }
+                    i += 1;
+                    continue;
+                } else if (bits[i] == 1) {
+                    if (bits[i + 1] == 1) {
+                        i += 2;
+                        continue;
+                    } else if (bits[i + 1] == 0) {
+                        i += 2;
+                        continue;
+                    }
+                }
+                i += 1;
+            }
+            return true;
+        }
     }
 }
