667db7befcb383b3336985ae5133e108a28965b0
laopo001 <353272497@qq.com>
2021-08-20 18:13:33 +0800

    fix

--- src/course_schedule_ii.rs
+++ src/course_schedule_ii.rs
@@ -11,11 +11,37 @@ fn is_all_zero(arr: &Vec<i32>) -> bool {
     return true;
 }
 
-fn run(start: i32, matrix: &Vec<Vec<i32>>) -> Vec<i32> {
+fn run(
+    start: i32,
+    matrix: &Vec<Vec<i32>>,
+    is_run: &mut Vec<i32>,
+    stop: &mut bool,
+) -> Vec<Vec<i32>> {
     let mut res = vec![];
-    res.push(start);
+    if (*stop) {
+        return vec![];
+    }
+    if (is_run[start as usize] == 1) {
+        *stop = true;
+        return vec![];
+    }
+
+    is_run[start as usize] = 1;
+    // dbg!(&is_run);
     for (i, arr) in matrix.iter().enumerate() {
-        if (arr[start as usize] != 0) {}
+        if (arr[start as usize] != 0) {
+            let z = run(i as i32, matrix, is_run, stop);
+
+            for mut i in z {
+                let mut x = vec![start];
+                x.append(&mut i);
+                res.push(x);
+            }
+        }
+    }
+    is_run[start as usize] = 0;
+    if (res.len() == 0) {
+        res.push(vec![start]);
     }
     return res;
 }
@@ -26,10 +52,55 @@ impl Solution {
         for i in prerequisites.iter() {
             matrix[i[0] as usize][i[1] as usize] = 1;
         }
-        // dbg!(&matrix);
-
+        let mut s = vec![];
+        let mut is_run = vec![0; num_courses as usize];
+        let mut stop = false;
         for (i, arr) in matrix.iter().enumerate() {
-            if (is_all_zero(arr)) {}
+            if (is_all_zero(arr)) {
+                let z = run(i as i32, &matrix, &mut is_run, &mut stop);
+                for i in z {
+                    s.push(i);
+                }
+            }
+        }
+        if (stop) {
+            return vec![];
+        }
+        dbg!(&s);
+        if (s.len() == 1) {
+            return s[0].clone();
+        } else {
+            is_run = vec![0; num_courses as usize];
+            let mut res = vec![];
+            let mut i = 0;
+            for x in s {
+                if (i == 0) {
+                    for y in x.iter() {
+                        is_run[*y as usize] = 1;
+                    }
+                    res = x;
+                    i += 1;
+                    continue;
+                }
+
+                for i in (0..x.len()).rev() {
+                    let y = x[i] as usize;
+                    if (is_run[y] == 0) {
+                        if (i == x.len() - 1) {
+                            res.push(y as i32);
+                        } else {
+                            let start = x[i + 1];
+                            let index = res.iter().position(|&r| r == start).unwrap();
+                            
+                            res.splice((index)..(index), [y as i32].iter().cloned());
+                        }
+                        is_run[y] = 1;
+                    }
+                }
+                dbg!(&res);
+                i += 1;
+            }
+            return res;
         }
         return vec![];
     }
@@ -37,6 +108,22 @@ impl Solution {
 
 #[test]
 fn test() {
-    let mut arr = vec![vec![1, 0], vec![2, 0], vec![3, 1], vec![3, 2]];
-    Solution::find_order(4, arr);
+    // let mut arr = vec![
+    //     vec![1, 0],
+    //     vec![0, 3],
+    //     vec![0, 2],
+    //     vec![3, 2],
+    //     vec![2, 5],
+    //     vec![4, 5],
+    //     vec![5, 6],
+    //     vec![2, 4],
+    // ];
+    // let res = Solution::find_order(7, arr);
+    let mut arr = vec![
+        vec![5,8],vec![3,5],vec![1,9],vec![4,5],vec![0,2],vec![7,8],vec![4,9]
+    ];
+    let res = Solution::find_order(10, arr);
+
+
+    dbg!(res);
 }
