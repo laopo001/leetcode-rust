39814f01bb7aa45d3d826d8e3a3d13c2ffd46e29
laopo001 <353272497@qq.com>
2023-06-15 20:15:18 +0800

    fix

---
+++ src/design-cancellable-function/description.js
@@ -0,0 +1,61 @@
+var cancellable = function (generator) {
+    let cancel;
+    let generatorValue;
+    let step = 0;
+    let promise = new Promise((resolve, reject) => {
+      cancel = () => {
+        if (step === 0) {
+          // reject('Cancelled');
+          try {
+            run(generator.throw('Cancelled'));
+          } catch {
+            reject('Cancelled');
+          }
+        } else {
+          resolve(generatorValue);
+        }
+      };
+      function run(result) {
+        try {
+          if (result.done) {
+            resolve(result.value);
+          } else {
+            if (result.value instanceof Promise) {
+              result.value
+                .then((res) => {
+                  generatorValue = res;
+                  step++;
+                  run(generator.next(res));
+                })
+                .catch((err) => {
+                  try {
+                    run(generator.throw(err));
+                  } catch {
+                    reject(err);
+                  }
+                });
+            } else {
+              generatorValue = result.value;
+              step++;
+              run(generator.next());
+            }
+          }
+        } catch (err) {
+          reject(err);
+        }
+      }
+      run(generator.next());
+    });
+    return [cancel, promise];
+  };
+
+/**
+ * function* tasks() {
+ *   const val = yield new Promise(resolve => resolve(2 + 2));
+ *   yield new Promise(resolve => setTimeout(resolve, 100));
+ *   return val + 1;
+ * }
+ * const [cancel, promise] = cancellable(tasks());
+ * setTimeout(cancel, 50);
+ * promise.catch(console.log); // logs "Cancelled" at t=50ms
+ */
